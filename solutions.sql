64be663f424685344028af531c84448f8e55536d
QinnniQ <nicholai.gay@gmail.com>
2025-07-29 15:26:45 +0200

    Create solutions.sql

---
+++ solutions.sql
@@ -0,0 +1,51 @@
+-- Challenge 1: Who Has Published What and Where?
+SELECT 
+    a.au_id AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    t.title AS "TITLE",
+    p.pub_name AS "PUBLISHER"
+FROM titleauthor ta
+JOIN authors a ON ta.au_id = a.au_id
+JOIN titles t ON ta.title_id = t.title_id
+JOIN publishers p ON t.pub_id = p.pub_id;
+
+-- Challenge 2: Who Has Published How Many and Where?
+SELECT 
+    a.au_id AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    p.pub_name AS "PUBLISHER",
+    COUNT(*) AS "TITLE COUNT"
+FROM titleauthor ta
+JOIN authors a ON ta.au_id = a.au_id
+JOIN titles t ON ta.title_id = t.title_id
+JOIN publishers p ON t.pub_id = p.pub_id
+GROUP BY a.au_id, p.pub_name;
+
+-- Challenge 3: Top 3 best selling authors by total quantity sold
+SELECT 
+    a.au_id AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    SUM(s.qty) AS "TOTAL"
+FROM authors a
+JOIN titleauthor ta ON a.au_id = ta.au_id
+JOIN titles t ON ta.title_id = t.title_id
+JOIN sales s ON t.title_id = s.title_id
+GROUP BY a.au_id
+ORDER BY TOTAL DESC
+LIMIT 3;
+
+-- Challenge 4: All authors ranked by total quantity sold
+SELECT 
+    a.au_id AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    IFNULL(SUM(s.qty), 0) AS "TOTAL"
+FROM authors a
+LEFT JOIN titleauthor ta ON a.au_id = ta.au_id
+LEFT JOIN titles t ON ta.title_id = t.title_id
+LEFT JOIN sales s ON t.title_id = s.title_id
+GROUP BY a.au_id
+ORDER BY TOTAL DESC;
