991481e37ad642fcafbf4c1a2df70d49ca0ab04d
ysassmann <ysassmann@googlemail.com>
2025-10-14 21:04:55 +0200

    2025-10-14: Completed lab-mysql-select

---
+++ solution.sql
@@ -0,0 +1,63 @@
+SELECT
+	a.au_id AS "AUTHOR ID",
+	a.au_lname AS "LAST NAME",
+	a.au_fname AS "FIRST NAME",
+	t.title AS "TITLE",
+	p.pub_name AS "PUBLISHER"
+FROM authors a
+JOIN titleauthor ta ON a.au_id = ta.au_id
+JOIN titles t ON ta.title_id = t.title_id
+JOIN publishers p ON t.pub_id = p.pub_id;
+
+
+SELECT
+	a.au_id AS "AUTHOR ID",
+	a.au_lname AS "LAST NAME",
+	a.au_fname AS "FIRST NAME",
+	t.title AS "TITLE",
+	p.pub_name AS "PUBLISHER",
+	COUNT(t.title) AS "TOTAL_COUNT"
+FROM titleauthor AS ta
+JOIN authors a ON a.au_id = ta.au_id
+JOIN titles t ON ta.title_id = t.title_id
+JOIN publishers p ON t.pub_id = p.pub_id
+JOIN sales s ON s.title_id = ta.title_id
+GROUP BY a.au_id, a.au_lname, p.pub_name;
+
+
+SELECT
+	a.au_id AS "AUTHOR ID",
+	a.au_lname AS "LAST NAME",
+	a.au_fname AS "FIRST NAME",
+	t.title AS "TITLE",
+	p.pub_name AS "PUBLISHER",
+	SUM(s.qty) AS "TOTAL"
+FROM titleauthor AS ta
+JOIN authors a ON a.au_id = ta.au_id
+JOIN titles t ON ta.title_id = t.title_id
+JOIN publishers p ON t.pub_id = p.pub_id
+JOIN sales s ON s.title_id = ta.title_id
+GROUP BY a.au_id, a.au_lname, p.pub_name
+ORDER BY "TOTAL" DESC
+LIMIT 3;
+
+
+SELECT
+	a.au_id AS "AUTHOR ID",
+	a.au_lname AS "LAST NAME",
+	a.au_fname AS "FIRST NAME",
+	t.title AS "TITLE",
+	p.pub_name AS "PUBLISHER",
+	COALESCE(SUM(s.qty), 0) AS "TOTAL"
+FROM titleauthor AS ta
+JOIN authors a ON a.au_id = ta.au_id
+JOIN titles t ON ta.title_id = t.title_id
+JOIN publishers p ON t.pub_id = p.pub_id
+LEFT JOIN sales s ON s.title_id = ta.title_id
+GROUP BY a.au_id, a.au_lname, p.pub_name
+ORDER BY "TOTAL" DESC;
+
+
+
+
+
