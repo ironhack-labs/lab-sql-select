29793de041013e53a6c70f223b0180027cf96a33
Suzana <250576254+suzanacracco-max@users.noreply.github.com>
2026-01-27 15:56:13 +0000

    Solutions SQL select

--- solutions.sql
+++ solutions.sql
@@ -44,41 +44,32 @@ ORDER BY
 
 Challange 3
 
-SELECT
-  a.au_id     AS "AUTHOR ID",
-  a.au_lname  AS "LAST NAME",
-  a.au_fname  AS "FIRST NAME",
-  SUM(s.qty)  AS "TOTAL"
-FROM authors a
-JOIN titleauthor ta
-  ON a.au_id = ta.au_id
-JOIN titles t
-  ON ta.title_id = t.title_id
-JOIN sales s
-  ON t.title_id = s.title_id
-GROUP BY
-  a.au_id,
-  a.au_lname,
-  a.au_fname
-ORDER BY
-  TOTAL DESC
+SELECT authors.au_id   AS "AUTHOR ID",
+authors.au_lname       AS "LAST NAME",
+authors.au_fname       AS "FIRST NAME",
+sum (titles.ytd_sales) AS "TOTAL"
+FROM authors
+JOIN titleauthor 
+  ON titleauthor.au_id = authors.au_id
+JOIN titles  
+  ON titles.title_id=titleauthor.title_id
+GROUP BY "AUTHOR ID"
+ORDER BY "TOTAL" DESC
 LIMIT 3;
 
 Challange 4
 
-SELECT
-  a.au_id               AS "AUTHOR ID",
-  a.au_lname            AS "LAST NAME",
-  a.au_fname            AS "FIRST NAME",
-  IFNULL(SUM(s.qty), 0) AS "TOTAL"
-FROM authors a
-LEFT JOIN titleauthor ta
-  ON a.au_id = ta.au_id
-LEFT JOIN titles t
-  ON ta.title_id = t.title_id
-LEFT JOIN sales s
-  ON t.title_id = s.title_id
-GROUP BY
-  a.au_id, a.au_lname, a.au_fname
-ORDER BY
-  TOTAL DESC;
+SELECT authors.au_id    AS "AUTHOR ID",
+authors.au_lname        AS "LAST NAME",
+authors.au_fname        AS "FIRST NAME",
+CASE
+	WHEN sum(titles.ytd_sales) IS NULL THEN 0
+	ELSE sum(titles.ytd_sales)
+END AS "TOTAL"
+FROM authors
+LEFT JOIN titleauthor 
+  ON titleauthor.au_id = authors.au_id
+LEFT JOIN titles  
+  ON titles.title_id=titleauthor.title_id
+GROUP BY "AUTHOR ID"
+ORDER BY "TOTAL" DESC;
