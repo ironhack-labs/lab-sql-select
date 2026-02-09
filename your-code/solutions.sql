2abea341a79447fe17c56470b415ab5867a9f655
marcosfsousa <80783407+marcosfsousa@users.noreply.github.com>
2026-02-09 15:26:24 +0100

    Added solutions for SQL challenges

---
+++ your-code/solutions.sql
@@ -0,0 +1,67 @@
+--Challenge 1
+
+SELECT 
+	authors.au_id AS "Author ID", 
+	authors.au_lname AS "Last name", 
+	authors.au_fname AS "First Name", 
+	titles.title AS "Title", 
+	publishers.pub_name as "Publisher"
+FROM titles, authors, publishers, titleauthor
+WHERE
+	titles.pub_id == publishers.pub_id AND
+	titles.title_id == titleauthor.title_id AND
+	titleauthor.au_id == authors.au_id
+	ORDER BY authors.au_id ASC
+;
+
+-- Challenge 2
+
+SELECT 
+	authors.au_id AS "Author ID", 
+	authors.au_lname AS "Last name", 
+	authors.au_fname AS "First Name", 
+	publishers.pub_name as "Publisher",
+	count(titles.title) as "Title Count"
+FROM titles, authors, publishers, titleauthor
+WHERE
+	titles.pub_id == publishers.pub_id AND
+	titles.title_id == titleauthor.title_id AND
+	titleauthor.au_id == authors.au_id
+GROUP by authors.au_id, publishers.pub_id
+order by authors.au_id DESC
+;
+
+-- Challenge 3
+
+SELECT
+	authors.au_id as "Author ID",
+	authors.au_lname as "Last Name",
+	authors.au_fname as "First Name",
+	sum(titles.price * sales.qty) as "Total"
+FROM sales, titles, titleauthor, authors
+WHERE
+	sales.title_id == titles.title_id AND
+	titles.title_id == titleauthor.title_id AND
+	titleauthor.au_id == authors.au_id
+GROUP by authors.au_id
+ORDER by "Total Sales" DESC
+LIMIT 3
+;
+
+--Challenge 4
+
+SELECT
+	authors.au_id as "Author ID",
+	authors.au_lname as "Last Name",
+	authors.au_fname as "First Name",
+	total(titles.price * sales.qty) as "Total Sales"
+FROM authors
+LEFT JOIN titleauthor
+		ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles
+		ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales
+		ON titles.title_id = sales.title_id
+GROUP by authors.au_id
+ORDER by "Total Sales" DESC
+;
