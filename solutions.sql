041a56b82ce1c28c794b4c44a47c17ec6fa151b0
Anastasia Kabanovsky <124449724+keirialaa@users.noreply.github.com>
2026-04-04 13:00:35 +0200

    Lab submission SQL select

---
+++ solutions.sql
@@ -0,0 +1,40 @@
+-- Challenge 1
+SELECT authors.au_id AS 'AUTHOR ID',  authors.au_lname AS 'LAST NAME',  authors.au_fname AS 'FIRST NAME', titles.title AS 'TITLE', publishers.pub_name AS 'PUBLISHER'
+FROM authors 
+JOIN titleauthor ON authors.au_id == titleauthor.au_id
+JOIN titles ON titleauthor.title_id == titles.title_id
+JOIN publishers ON titles.pub_id == publishers.pub_id;
+
+-- Challenge 2
+SELECT 
+    authors.au_id AS 'AUTHOR ID', 
+    authors.au_lname AS 'LAST NAME', 
+    authors.au_fname AS 'FIRST NAME', 
+    publishers.pub_name AS 'PUBLISHER',
+    COUNT(titles.title) AS 'TITLE COUNT'
+FROM authors 
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN titles ON titleauthor.title_id = titles.title_id
+JOIN publishers ON titles.pub_id = publishers.pub_id
+GROUP BY 
+    authors.au_id, 
+    authors.au_lname, 
+    authors.au_fname, 
+    publishers.pub_name
+ORDER BY COUNT(titles.title) DESC;
+
+-- Challenge 3
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', COUNT(titles.title) AS 'TOTAL' 
+FROM authors 
+JOIN titleauthor ON authors.au_id == titleauthor.au_id JOIN titles ON titleauthor.title_id == titles.title_id
+GROUP BY authors.au_id
+ORDER BY COUNT(titles.title) DESC LIMIT 3;
+
+-- Challenge 4
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', COUNT(titles.title) AS 'TOTAL' 
+FROM authors 
+JOIN titleauthor ON authors.au_id == titleauthor.au_id JOIN titles ON titleauthor.title_id == titles.title_id
+GROUP BY authors.au_id
+ORDER BY COUNT(titles.title) DESC;
+
+
