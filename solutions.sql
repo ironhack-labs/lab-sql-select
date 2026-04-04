5f4c867612dd8ab20924205732adb022b1257fb3
Anastasia Kabanovsky <124449724+keirialaa@users.noreply.github.com>
2026-04-04 13:05:00 +0200

    Lab submission SQL select correction

--- solutions.sql
+++ solutions.sql
@@ -31,10 +31,11 @@ GROUP BY authors.au_id
 ORDER BY SUM(titles.ytd_sales) DESC LIMIT 3;
 
 -- Challenge 4
-SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', SUM(titles.ytd_sales) AS 'TOTAL' 
-FROM authors 
-JOIN titleauthor ON authors.au_id == titleauthor.au_id JOIN titles ON titleauthor.title_id == titles.title_id
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', COALESCE(SUM(titles.ytd_sales), 0) AS 'TOTAL'
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
 GROUP BY authors.au_id
-ORDER BY SUM(titles.ytd_sales)DESC;
+ORDER BY SUM(titles.ytd_sales) DESC;
 
 
