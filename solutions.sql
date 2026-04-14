755044338edb05f942c68e1617281a272629950e
Alan An Jung Wei <alanweica@gmail.com>
2026-04-14 15:17:52 +0200

    Mistake fixed

--- solutions.sql
+++ solutions.sql
@@ -8,7 +8,7 @@ JOIN publishers AS p ON t.pub_id = p.pub_id;
 
 -- Challenge 2 - Who have published how many at where? ---
 
-SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME', a.au_fname AS 'FIRST NAME', p.pub_name AS 'PUBLISHER', SUM(ta.au_ord) AS 'TITLE COUNT'
+SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME', a.au_fname AS 'FIRST NAME', p.pub_name AS 'PUBLISHER', COUNT(ta.title_id) AS 'TITLE COUNT'
 FROM authors AS a
 JOIN titleauthor AS ta ON a.au_id = ta.au_id
 JOIN titles AS t ON ta.title_id = t.title_id 
@@ -17,7 +17,7 @@ GROUP BY p.pub_name, a.au_id;
 
 -- Challenge 3 - Best Selling authors ---
 
-SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME', a.au_fname AS 'FIRST NAME', SUM(ta.au_ord) AS 'TOTAL'
+SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME', a.au_fname AS 'FIRST NAME', COUNT(ta.title_id) AS 'TOTAL'
 FROM authors AS a
 JOIN titleauthor AS ta ON a.au_id = ta.au_id
 JOIN titles AS t ON ta.title_id = t.title_id 
@@ -28,7 +28,7 @@ LIMIT 3;
 
 -- Challenge 4 - Best Selling Authors Ranking ---
 
-SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME', a.au_fname AS 'FIRST NAME', IFNULL(SUM(ta.au_ord), 0) AS 'TOTAL'
+SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME', a.au_fname AS 'FIRST NAME', IFNULL(COUNT(ta.title_id), 0) AS 'TOTAL'
 FROM authors AS a
 LEFT JOIN titleauthor AS ta ON a.au_id = ta.au_id
 LEFT JOIN titles AS t ON ta.title_id = t.title_id 
