b68a5be378ec3f02471062c0a3d18bfe8fa91e5b
yoanl13 <yoanliu@gmail.com>
2026-06-30 12:49:37 +0200

    sql pending q4

---
+++ your-code/solutions.sql
@@ -0,0 +1,30 @@
+SELECT a.au_id AS AUTHOR_ID, a.au_lname AS LAST_NAME, a.au_fname AS FIRST_NAME, t.title AS TITLE, p.pub_name AS PUBLISHER
+FROM authors AS a
+JOIN titleauthor AS ta ON a.au_id = ta.au_id
+JOIN titles AS t ON ta.title_id = t.title_id
+JOIN publishers AS p ON t.pub_id = p.pub_id;--Q1
+
+SELECT a.au_id AS AUTHOR_ID, a.au_lname AS LAST_NAME, a.au_fname AS FIRST_NAME, p.pub_name AS PUBLISHER, count(t.title) AS TITLE_COUNT
+FROM authors AS a
+JOIN titleauthor AS ta ON a.au_id = ta.au_id
+JOIN titles AS t ON ta.title_id = t.title_id
+JOIN publishers AS p ON t.pub_id = p.pub_id
+GROUP BY a.au_id, p.pub_name
+ORDER BY count(t.title)DESC, a.au_id DESC;--Q2
+
+SELECT a.au_id AS AUTHOR_ID, a.au_lname AS LAST_NAME, a.au_fname AS FIRST_NAME, sum(s.qty) AS TOTAL
+FROM authors AS a
+JOIN titleauthor AS ta ON a.au_id = ta.au_id
+JOIN titles AS t ON ta.title_id = t.title_id
+JOIN sales AS s ON t.title_id = s.title_id
+GROUP BY a.au_id
+ORDER BY sum(s.qty)DESC
+LIMIT 3;--Q3
+
+SELECT a.au_id AS AUTHOR_ID, a.au_lname AS LAST_NAME, a.au_fname AS FIRST_NAME, sum(s.qty) AS TOTAL
+FROM authors AS a
+JOIN titleauthor AS ta ON a.au_id = ta.au_id
+JOIN titles AS t ON ta.title_id = t.title_id
+JOIN sales AS s ON t.title_id = s.title_id
+GROUP BY a.au_id	
+ORDER BY sum(s.qty)DESC;--Q4
